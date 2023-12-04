bba2d95092242ce200bd3a78d081ed79500b8e49
ludelanier <lucas.delanier@etu.uca.fr>
2023-12-04 21:16:43 +0100

    add scroll button for computer with mouse

--- src/app/ui/components/CollectionRow.tsx
+++ src/app/ui/components/CollectionRow.tsx
@@ -1,24 +1,47 @@
-
 import styles from "@/app/ui/app.module.css";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Movie from "@/app/classes/Movie";
 import MovieCollectionCell from "@/app/ui/components/MovieCollectionCell";
 import Image from "next/image";
 
 interface props {
-    title: string
-    fetchFunction: (page : number) => Promise<Movie[]>;
+    title: string;
+    fetchFunction: (page: number) => Promise<Movie[]>;
 }
 
-export function CollectionRow({title, fetchFunction}: props) {
+export function CollectionRow({ title, fetchFunction }: props) {
     const [page, setPage] = useState<number>(1);
-    const [movies, setMovies] = useState<Movie[]>([])
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [isAtMaxScroll, setIsAtMaxScroll] = useState<boolean>(false);
+    const [isAtMinScroll, setIsAtMinScroll] = useState<boolean>(true);
+    const collectionRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
-        fetchData()
-    }, [])
+        fetchData();
+    }, []);
 
     function handleAdd() {
-        fetchData()
+        fetchData();
+    }
+
+    function scrollCollection(left: boolean) {
+        if (collectionRef.current) {
+            const container = collectionRef.current;
+            container.scrollBy({
+                left: left ? -1300 : 1300,
+                behavior: 'smooth',
+            });
+        }
+    }
+
+    function isAtMaxOrMinScroll() {
+        console.log(collectionRef.current?.scrollLeft)
+        console.log(collectionRef.current?.scrollWidth! - 1100)
+        if (collectionRef.current) {
+            const container = collectionRef.current;
+            setIsAtMaxScroll(container.scrollLeft >= (container.scrollWidth- container.clientWidth - 100));
+            setIsAtMinScroll(container.scrollLeft <= 100);
+        }
     }
 
     function fetchData() {
@@ -33,23 +56,34 @@ export function CollectionRow({title, fetchFunction}: props) {
             setPage((prevPage) => prevPage + 1);
         });
     }
+
     return (
         <div className={styles.collectionContainer}>
             <h1 className={styles.collectionTitle}>{title}</h1>
 
-                <div className={`flex gap-2 pl-20 pr-20 overflow-x-scroll overflow-y-visible ${styles.spacingRow}`}>
-                    {movies.map((movie: Movie) => (
-                        <MovieCollectionCell movie={movie} key={movie.id} />
-                    ))}
-                    <Image
-                        src="/loadMoreButton.svg"
-                        width={259}
-                        height={389}
-                        alt={"poster"}
-                        className={styles.addButton}
-                        onClick={handleAdd}
-                    />
+            <div
+                ref={collectionRef}
+                className={`flex gap-2 pl-20 pr-20 overflow-x-scroll overflow-y-visible ${styles.spacingRow}`}
+                onScroll={isAtMaxOrMinScroll}
+            >
+                {movies.map((movie: Movie) => (
+                    <MovieCollectionCell movie={movie} key={movie.id} />
+                ))}
+                <div className={styles.scrollButtonContainer} style={{ display: isAtMaxScroll ? 'none' : 'flex' }} onClick={() => { scrollCollection(false) }}>
+                    <Image className={styles.scrollButton} src={"/right-scroll.png"} alt={"scroll right"} width={50} height={50}></Image>
                 </div>
+                <div className={styles.scrollButtonContainerLeft} style={{ display: isAtMinScroll ? 'none' : 'flex' }} onClick={() => { scrollCollection(true) }}>
+                    <Image className={`${styles.scrollButton} ${styles.left}`} src={"/right-scroll.png"} alt={"scroll left"} width={50} height={50}></Image>
+                </div>
+                <Image
+                    src="/loadMoreButton.svg"
+                    width={259}
+                    height={389}
+                    alt={"poster"}
+                    className={styles.addButton}
+                    onClick={handleAdd}
+                />
+            </div>
         </div>
     );
 }
--- src/app/ui/components/ShowCaseCollection.tsx
+++ src/app/ui/components/ShowCaseCollection.tsx
@@ -1,5 +1,5 @@
 import styles from "@/app/ui/app.module.css";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Movie from "@/app/classes/Movie";
 import ShowCaseCell from "@/app/ui/components/ShowCaseCell";
 import Image from "next/image";
@@ -12,12 +12,36 @@ interface props {
 export function ShowCaseCollection({title, fetchFunction}: props) {
     const [page, setPage] = useState<number>(1);
     const [movies, setMovies] = useState<Movie[]>([])
+    const [isAtMaxScroll, setIsAtMaxScroll] = useState<boolean>(false);
+    const [isAtMinScroll, setIsAtMinScroll] = useState<boolean>(true);
+    const collectionRef = useRef<HTMLDivElement>(null);
+
     useEffect(() => {
         fetchData()
     }, [])
 
     function handleAdd() {
         fetchData()
+    }
+
+    function scrollCollection(left: boolean) {
+        if (collectionRef.current) {
+            const container = collectionRef.current;
+            container.scrollBy({
+                left: left ? -(container.clientWidth-180) : container.clientWidth - 180,
+                behavior: 'smooth',
+            });
+        }
+    }
+
+    function isAtMaxOrMinScroll() {
+        console.log(collectionRef.current?.scrollLeft)
+        console.log(collectionRef.current?.scrollWidth! - 1100)
+        if (collectionRef.current) {
+            const container = collectionRef.current;
+            setIsAtMaxScroll(container.scrollLeft >= (container.scrollWidth- container.clientWidth - 100));
+            setIsAtMinScroll(container.scrollLeft <= 100);
+        }
     }
 
     function fetchData() {
@@ -37,10 +61,16 @@ export function ShowCaseCollection({title, fetchFunction}: props) {
     return (
         <div className={styles.collectionContainer}>
             <h1 className={styles.collectionTitle}>{title}</h1>
-            <div className={`flex gap-2 pl-20 pr-20 overflow-x-scroll overflow-y-hidden ${styles.spacingRow}`}>
+            <div ref={collectionRef} onScroll={isAtMaxOrMinScroll} className={`flex gap-2 pl-20 pr-20 overflow-x-scroll overflow-y-hidden ${styles.spacingRow}`}>
                 {movies.map((movie: Movie, idx) => (
                     <ShowCaseCell movie={movie} key={idx}/>
                 ))}
+                <div className={styles.scrollButtonContainer} style={{ display: isAtMaxScroll ? 'none' : 'flex' }} onClick={() => { scrollCollection(false) }}>
+                    <Image className={styles.scrollButton} src={"/right-scroll.png"} alt={"scroll right"} width={50} height={50}></Image>
+                </div>
+                <div className={styles.scrollButtonContainerLeft} style={{ display: isAtMinScroll ? 'none' : 'flex' }} onClick={() => { scrollCollection(true) }}>
+                    <Image className={`${styles.scrollButton} ${styles.left}`} src={"/right-scroll.png"} alt={"scroll left"} width={50} height={50}></Image>
+                </div>
                 <Image
                     src="/loadMoreButton.svg"
                     width={259}
