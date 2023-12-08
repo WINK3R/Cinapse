8baa76d7cec7adbbafe033e3d03fe298c4023c09
ludelanier <lucas.delanier@etu.uca.fr>
2023-12-08 22:02:33 +0100

    add carousel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import React from "react";
 import NavBar from "@/app/ui/components/NavBar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function RootLayout({
   children,
